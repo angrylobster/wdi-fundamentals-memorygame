bbf07bd2dca1580cd863fa5bc32402d6356edc5a
angrylobster <liewminshan@gmail.com>
2018-11-02 12:59:43 +0800

    Create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne + " and " + cardTwo);
+var cards = ["queen", "king", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[3];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+for (var i = 0; i < cardsInPlay.length; i++) {
+	console.log("User flipped " + cardsInPlay[i]);
+}
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!");
+	}
+}
